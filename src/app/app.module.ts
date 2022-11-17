043d9cd036027918177cb7b514cd169a775eb0b9
jianyilai <2002854D@student.tp.edu.sg>
2022-11-17 21:10:57 +0800

    Added To-Do Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,21 @@ import { IonicModule } from "@ionic/angular";
 import { SettingsComponent } from './settings/settings.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ToDoComponent } from './to-do/to-do.component';
+import { RemindersComponent } from './reminders/reminders.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SettingsComponent,
     NavBarComponent,
-    LoginComponent
+    LoginComponent,
+    SignUpComponent,
+    ProfileComponent,
+    ToDoComponent,
+    RemindersComponent
   ],
   imports: [
     BrowserModule,
