589fa610dfb2cbc8a4be87007f10d4e2bec0c0d3
jianyilai <2002854D@student.tp.edu.sg>
2022-11-21 18:01:57 +0800

    Update server.js and api.js

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,22 +1,57 @@
 const express = require('express');
 const router = express.Router();
+const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectID;
+
+const bcrypt = require('bcryptjs');
+const BCRYPT_SALT_ROUNDS = 12;
+
 // declare axios for making http requests
 const axios = require('axios');
-const API = 'https://jsonplaceholder.typicode.com';
-/* GET api listing. */
-router.get('/', (req, res) => {
-    res.send('api works');
+var db;
+
+MongoClient.connect('mongodb+srv://testuser:testpass@cluster0.wtnbhkz.mongodb.net/?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, (err, database) => {
+    if (err) return console.log(err);
+    db = database.db('coaching-chatbot');
 });
-// Get all posts
-router.get('/posts', (req, res) => {
-    // Get posts from the mock api
-    // This should ideally be replaced with a service that connects to MongoDB
-    axios.get(`${API}/posts`)
-        .then(posts => {
-            res.status(200).json(posts.data);
-        })
-        .catch(error => {
-            res.status(500).send(error)
+
+router.route('/authuser').post(function (req, res2) {
+    var username = req.body.username;
+    var password = req.body.password;
+    db.collection('users').findOne({ "name": username }, {
+        password: 1, role: 1,
+        _id: 0
+    }, function (err, result) {
+        if (result == null) res2.send([{ "auth": false }]);
+        else {
+            bcrypt.compare(password, result.password, function (err, res) {
+                if (err || res == false) {
+                    res2.send([{ "auth": false }]);
+                } else {
+                    res2.send([{ "auth": true, "role": result.role }]);
+                }
+            });
+        }
+    });
+});
+router.route('/reguser').post(function (req, res) {
+    var username = req.body.username;
+    var email = req.body.email;
+    var password = req.body.password;
+    var role = req.body.role;
+    bcrypt.hash(password, BCRYPT_SALT_ROUNDS, function (err, hash) {
+        db.collection('users').insertOne({
+            "name": username, "email": email, "password": hash,
+            "role": role
+        }, (err, result) => {
+            if (err) return console.log(err)
+            console.log('user registered')
+            res.send(result);
         });
-});
+    });
+})
+
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use(express.static(path.join(__dirname, '../dist')));
 app.use('/api', api);
 // Catch all other routes and return the index file
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../dist/meanApp/index.html'));
+    res.sendFile(path.join(__dirname, '../dist/authApp/index.html'));
 });
 /**
 * Get port from environment and store in Express.
